e2652d0c8f187850d1c0c7f56dbe6ecf04e67e67
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-06-15 11:43:59 -0300

    summary

---
+++ exerciciospraticos/ex1.js
@@ -0,0 +1,17 @@
+let points = 3
+
+function dec() {
+  points -= 1
+}
+
+function add() {
+  points += 3
+}
+
+add()
+add()
+add()
+dec()
+dec()
+
+console.log(points)
--- passangercounter/index.js
+++ passangercounter/index.js
@@ -1,6 +1,16 @@
-// document.getElementById("count-el").innerText = 5
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-  
-} 
+  count = count + 1
+  countEl.innerHTML = count
+}
+
+function save() {
+  let countString = count + " - "
+  saveEl.innerHTML += countString
+  count = 0
+  countEl.innerHTML = count
+}
 
