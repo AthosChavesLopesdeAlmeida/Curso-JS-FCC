0e4c6b3d9107f5589f57425ce9ca6ff9d0c21f8f
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-06-21 17:43:17 -0300

    blackjack

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,21 +1,28 @@
 
 let c1 = 11
-let c2 = 10
+let c2 = 11
 let sum = c1 + c2
 let hasBlackJack = false
 let isAlive = true
 message = " "
 
-console.log(sum)
+let messageEL = document.getElementById("msg-el")
+let sumEL = document.getElementById("sum-el")
+let cardsEL = document.getElementById("cards-el")
 
-if (sum <= 20){
-  message = "You can draw a new card"
-} else if(sum == 21) {    
-  hasBlackJack = true
-  message = "Blackjack"
-} else {
-  message = "You lose. Fuck you"
-  isAlive = false
+function gamestart() {
+    if (sum <= 20){
+    message = "You can draw a new card."
+  } else if(sum == 21) {    
+    hasBlackJack = true
+    message = "You got Blackjack!"
+  } else {
+    message = "You lose. Fuck you"
+    isAlive = false
+  }
+
+  messageEL.innerHTML = message
+  sumEL.innerHTML = "Sum: " + sum
+  cardsEL.innerHTML = `Sum: ${c1}, ${c2}`
 }
 
-console.log(message) 
