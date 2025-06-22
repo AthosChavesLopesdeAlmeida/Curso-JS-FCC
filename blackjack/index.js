478c162b081038adc199c33f30e86ae8899405ef
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-06-22 16:17:14 -0300

    ok

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,6 +1,7 @@
 
-let c1 = 11
-let c2 = 11
+let c1 = 10
+let c2 = 4
+let cards = [c1, c2]
 let sum = c1 + c2
 let hasBlackJack = false
 let isAlive = true
@@ -10,7 +11,12 @@ let messageEL = document.getElementById("msg-el")
 let sumEL = document.getElementById("sum-el")
 let cardsEL = document.getElementById("cards-el")
 
-function gamestart() {
+
+function startGame() {
+  renderGame()
+}
+
+function renderGame() {
     if (sum <= 20){
     message = "You can draw a new card."
   } else if(sum == 21) {    
@@ -23,6 +29,12 @@ function gamestart() {
 
   messageEL.innerHTML = message
   sumEL.innerHTML = "Sum: " + sum
-  cardsEL.innerHTML = `Sum: ${c1}, ${c2}`
+  cardsEL.innerHTML = `Sum: ${cards[0]}, ${cards[1]}`
 }
 
+function newcard() {
+  let newcard = 7
+  sum += newcard
+
+  renderGame()
+}
---
+++ exerciciospraticos/ex4.js
@@ -0,0 +1,5 @@
+let featuredposts = [
+  "Check out my Netflix clone", 
+  "Here's the code for my project", 
+  "I've relaunched my protfolio"
+]
