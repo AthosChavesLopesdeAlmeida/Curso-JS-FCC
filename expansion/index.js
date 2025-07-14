5730d459c35e5bcc193059cde09a8218d5e2f713
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-07-14 17:39:03 -0300

    ok

---
+++ expansion/index.js
@@ -0,0 +1,9 @@
+function saveLead(){
+  console.log("Button clicked 1")
+}
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+  console.log("Button clicked 2")
+})
