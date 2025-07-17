6097c83f16e784b67077ad4812dacb2ab7a62ce1
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-07-17 16:29:41 -0300

    Projeto finalizado

--- expansion/index.js
+++ expansion/index.js
@@ -1,9 +1,27 @@
-function saveLead(){
-  console.log("Button clicked 1")
-}
-
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const listEl = document.getElementById("listEl")
+let listItems = ""
 
 inputBtn.addEventListener("click", function(){
-  console.log("Button clicked 2")
+  myLeads.push(inputEl.value) 
+  inputEl.value = "" // Limpa o campo de entrada após adicionar o lead
+  renderLeads()
 })
+
+function renderLeads(){
+let listItems = ""
+for(let i = 0; i < myLeads.length; i++){
+  // const li = document.createElement("li")
+  // li.textContent = myLeads[i]
+  // listEl.append(li)
+  // Acima está outra maneira de criar elementos de lista
+
+  listItems += 
+  `<li>
+  <a target = '_blank' href ='${myLeads[i]}'>"  ${myLeads[i]}   "</a>
+  </li>`
+}
+listEl.innerHTML = listItems
+}
