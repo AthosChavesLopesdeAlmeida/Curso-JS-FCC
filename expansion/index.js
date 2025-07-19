dcba62582868ab62dd8c14bf3b0e6886ebd1a3c6
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-07-18 21:24:28 -0300

    JSON update

--- expansion/index.js
+++ expansion/index.js
@@ -2,26 +2,42 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const listEl = document.getElementById("listEl")
+const deleteBtn = document.getElementById("delete-btn")
 let listItems = ""
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) 
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+
 
 inputBtn.addEventListener("click", function(){
   myLeads.push(inputEl.value) 
-  inputEl.value = "" // Limpa o campo de entrada após adicionar o lead
+  inputEl.value = "" 
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
 })
 
 function renderLeads(){
 let listItems = ""
-for(let i = 0; i < myLeads.length; i++){
-  // const li = document.createElement("li")
-  // li.textContent = myLeads[i]
-  // listEl.append(li)
-  // Acima está outra maneira de criar elementos de lista
-
+  for(let i = 0; i < myLeads.length; i++){
   listItems += 
   `<li>
   <a target = '_blank' href ='${myLeads[i]}'>"  ${myLeads[i]}   "</a>
   </li>`
 }
-listEl.innerHTML = listItems
+  listEl.innerHTML = listItems
 }
+
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// listEl.append(li)
+// Acima está outra maneira de criar elementos de lista
